1581c87264e628b97508de4ffbdb653ec1b66587
KillianFournier <killianfournier0@gmail.com>
2021-10-22 09:07:57 +0200

    intru

---
+++ intru/.liste.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+l1="chasse,chasser,chasselas,chasseur,chassé"
+l2="affable,faible,faiblaisse,affaiblissement,faiblir"
+l3="chat,chien,tigre,lion,guépard"
+l4="palper,impalpable,palpation,impaludation,palpable"
+l5="renard,renauder,renardière,renardeau,renarde"
+
